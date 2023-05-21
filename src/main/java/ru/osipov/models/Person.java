abcbeb7333c7781c227619bd8c1970248ee1b91a
Dmitry Osipov <deosipov@mail.ru>
2023-05-21 19:27:23 +0300

    Init Commit

---
+++ src/main/java/ru/osipov/models/Person.java
@@ -0,0 +1,82 @@
+package ru.osipov.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty
+    @Size(min = 2, max = 30, message = "Имя должно быть длиной между 2 и 30 символов")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 0, message = "Возваст не может быть меньше нуля")
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Email не должен быть пустым")
+    @Email
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
